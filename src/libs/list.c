b7a43acfe8d67aaeeea238ae00e2799f02b4ba15
Baptiste Audugé <baptiste.auduge@gmail.com>
2023-04-06 01:47:35 +0200

    perf: optimized func

--- src/libs/list.c
+++ src/libs/list.c
@@ -1,11 +1,12 @@
 // Sorbonne University, 2022 - 2023
 // MyGit Project
-// 
+//
 // list.c
 // File description:
 //   This file contains the implementation of the list.h header.
 
 #include "list.h"
+#include "constants.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,7 +23,8 @@ List **create_init_list() {
 void free_list(List **list) {
   if (!list)
     return;
-  Cell *iter = *list, *tmp;
+  Cell *iter = *list;
+  Cell *tmp;
   while (iter) {
     tmp = iter;
     iter = iter->next;
@@ -45,7 +47,7 @@ Cell *get_index_of_list(List **list, int pos) {
   for (int i = 0; i < pos && iter; ++i)
     iter = iter->next;
   if (!iter)
-    fprintf(stderr, "[get_index_of_list] Index out of range\n");
+    fprintf(stderr, ERROR_INDEX_OUT_OF_RANGE);
   return iter;
 }
 
@@ -59,8 +61,7 @@ Cell *search_data_in_list(List **list, const char *str) {
 }
 
 int create_and_insert_cell_in_list(List **list, const char *data) {
-  Cell *new_cell = NULL;
-  new_cell = build_cell(data);
+  Cell *new_cell = build_cell(data);
   if (!new_cell)
     return 0;
   insert_cell_at_begging_list(list, new_cell);
